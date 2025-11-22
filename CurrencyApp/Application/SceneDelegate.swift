cb1a6cb6f03803cf4d82769c1091f5ce4a28a69f
artemsnoegin <snoegin@gmail.com>
2025-11-22 14:22:07 +0300

    init commit

---
+++ CurrencyApp/Application/SceneDelegate.swift
@@ -0,0 +1,30 @@
+//
+//  SceneDelegate.swift
+//  CurrencyApp
+//
+//  Created by Артём Сноегин on 22.11.2025.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        
+        guard let scene = (scene as? UIWindowScene) else { return }
+        
+        let window = UIWindow(windowScene: scene)
+        
+        let rootViewController = ViewController()
+        let navigationController = UINavigationController(rootViewController: rootViewController)
+        
+        window.rootViewController = navigationController
+        window.makeKeyAndVisible()
+        
+        self.window = window
+    }
+}
+
---
+++ CurrencyApp/UI/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  CurrencyApp
+//
+//  Created by Артём Сноегин on 22.11.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
+#Preview {
+    ViewController()
+}
