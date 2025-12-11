e831d21016e7c4407201acadda07b7d4668df12f
artemsnoegin <snoegin@gmail.com>
2025-12-11 19:36:12 +0300

    refactor currency model

--- CurrencyApp/UI/CurrencyPicker/Model/Currency.swift
+++ CurrencyApp/UI/CurrencyPicker/Model/Currency.swift
@@ -5,14 +5,31 @@
 //  Created by Артём Сноегин on 24.11.2025.
 //
 
-struct Currency: Decodable {
+struct Currency: Decodable, Equatable {
     
     let symbol: String
     let name: String
-    let symbol_native: String
-    let decimal_digits: Int
+    let symbolNative: String
+    let decimalDigits: Int
     let rounding: Int
     let code: String
-    let name_plural: String
+    let namePlural: String
     let type: String
+    
+    var exchangeRates: [String:Double]? = nil
+    
+    enum CodingKeys: String, CodingKey {
+        case symbol
+        case name
+        case symbolNative = "symbol_native"
+        case decimalDigits = "decimal_digits"
+        case rounding
+        case code
+        case namePlural = "name_plural"
+        case type
+    }
+    
+    static func ==(lhs: Currency, rhs: Currency) -> Bool {
+            return lhs.code == rhs.code
+        }
 }
